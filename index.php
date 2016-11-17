104653f1883769af51400c8215db72c7b80c9e25
Emixham <Maximed.ipod@gmail.com>
2016-11-17 11:15:40 +0100

    Index
    Oui

--- index.php
+++ index.php
@@ -1,6 +1,38 @@
+<<<<<<< HEAD
 index.php
 <html>
     <body>
 TEST
     </body>
 </html>
+=======
+<?php
+session_start();
+require_once("util/fonctions.inc.php");
+require_once("util/class.PdogestionRS.inc.php");
+include("vues/v_entete.php") ;
+include("vues/v_bandeau.php") ;
+
+if(!isset($_REQUEST['uc']))
+     $uc = 'accueil';
+else
+	$uc = $_REQUEST['uc'];
+
+$pdo = PdogestionRS::getPdogestionRS();
+switch($uc)
+{
+	case 'accueil':
+		{include("vues/v_accueil.php");break;}
+	case 'connexion' :
+		{ include("controleurs/c_connexion.php");break; }
+	case 'voirCategories' :
+		{include("controleurs/c_voirCategories.php");break;}
+	case 'voirSalle' :
+		{ include("controleurs/c_voirSalle.php");break; }
+	case 'reserverSalle' :
+	  { include("controleurs/c_reserverSalle.php");break;  }
+}
+include("vues/v_pied.php") ;
+?>     
+
+>>>>>>> origin/master
