2d66757a37a9c92a4e4495fee3414ad9b5f195f9
Emixham <Maximed.ipod@gmail.com>
2016-11-24 14:08:07 +0100

    Ajout de la connexion
    Controleur + vue de connexion

--- controleurs/c_connexion.php
+++ controleurs/c_connexion.php
@@ -1,6 +1,53 @@
 
 <?php
+$action = $_REQUEST['action'];
+switch($action)
+{
+	case 'seconnecter':
+	{
+		if(!isset($_SESSION['id']))
+			{
+				include("vues/v_connexion.php");
+			}
+		else
+			{
+				include("vues/v_deconnexion.php");
+			}
 
+
+	break;
+	}
+	case 'connexion':
+	{
+		$nom =$_POST['nom'];
+		$mdp =$_POST['mdp'];
+
+		$resultat = $pdo ->seConnecter($nom,$mdp);
+		if (!$resultat)
+		{
+		    $message="Nom de compte ou mot de passe incorect";
+			include("vues/v_message.php");
+		}
+		else
+		{
+		    
+		    $_SESSION['id'] = $resultat['id'];
+		    $_SESSION['nom'] = $nom;
+		    echo 'Vous êtes connecté !';
+		    header('location: index.php?uc=voirProduits&action=voirCategories');
+
+		}
+	break;
+	}
+	case 'deconnexion':
+	{
+		$_SESSION = array();
+		session_destroy();
+		header('location: index.php');
+
+	break;
+	}
+}
 
 
 
--- vues/v_connexion.php
+++ vues/v_connexion.php
@@ -3,11 +3,11 @@
 <form method="POST" action="index.php?uc=administrer&action=confirmer">
 	<p>
 	<label for="nom">Login :</label>
-		<input id="nom" type="text" name="nom" value="" size="50">
+		<input type="text" name="nom" id="nom" value="" size="50">
 	</p>
 	<p>
 	<label for="mdp">Mot de passe :</label>
-		<input id="mdp" type="text" name="mdp" value="" size="50">
+		<input  type="password" name="mdp" id="mdp" value="" size="50">
     </p>
         <input type="submit" value="Valider" name="valider">
         <input type="reset" value="Annuler" name="annuler"> 
